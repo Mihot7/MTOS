c8b7a077dffd4bbb2ce02b2aa3cece24f8a93e7a
Mihot7 <105102302+Mihot7@users.noreply.github.com>
2024-03-24 16:48:11 +0100

    updated to 2.1

--- boot.bat
+++ boot.bat
@@ -28,14 +28,31 @@ if exist %shell%.bat (
 :normal_boot
 cd..
 sys
+Echo ERROR! (E02)
+pause
+echo shutting down!
+timeout /t 5 /nobreak >nul
+exit
 
 :safe_boot
-set shell=2shell
+set shell=safe_shell
 echo UWAGA, SYSTEM USZKODZONY! WCHODZENIE W TRYB AWARYJNY. 
 timeout /t 2 /nobreak >nul
-echo Pobieranie pliku repair.bat (na wypadek jego uszkodzenia)
-del repair.bat /q
-curl https://raw.githubusercontent.com/Mihot7/MTOS/main/prog/repair.bat --silent --output repair.bat
+echo Pobieranie plików repair.bat oraz repair.cmd (na wypadek ich uszkodzenia)
+del repair.bat /q>nul
+del repair.cmd /q>nul
+curl https://raw.githubusercontent.com/Mihot7/MTOS-REPAIR-SERVER/main/%ver%/repair.bat --silent --output repair.bat
+curl https://raw.githubusercontent.com/Mihot7/MTOS-REPAIR-SERVER/main/%ver%/repair.cmd --silent --output repair.cmd
+timeout /t 2 /nobreak >nul
+echo Pobieranie pliku help.bat (na wypadek jego uszkodzenia)
+del safe_shell.bat /q>nul
+curl https://raw.githubusercontent.com/Mihot7/MTOS-REPAIR-SERVER/main/%ver%/help.bat --silent --output help.bat
+curl https://raw.githubusercontent.com/Mihot7/MTOS-REPAIR-SERVER/main/%ver%/2shell.bat --silent --output safe_shell.bat
 timeout /t 2 /nobreak >nul
 cd..
 call sys.bat
+Echo ERROR! (E01)
+pause
+echo shutting down!
+timeout /t 5 /nobreak >nul
+exit
--- prog/repair.bat
+++ prog/repair.bat
@@ -5,16 +5,7 @@ if %choose%==y goto repair
 if %choose%==n goto exit
 
 :repair
-echo Wpisz nazwe systemowego pakietu który nie działa. (wpisz "shell" jeśli chcesz go naprawić!)
-set /p rpkg=NAZWA:
-if %rpkg%==shell goto shell
-del %rpkg%.bat /q
-curl https://raw.githubusercontent.com/Mihot7/MTOS/main/prog/%rpkg%.bat --silent --output %rpkg%.bat
-echo Pakiet naprawiony!
-
-:shell
-del %shell%.bat /q
-curl https://raw.githubusercontent.com/Mihot7/MTOS/main/prog/%shell%.bat --silent --output %shell%.bat
+call repair.cmd
 
 :exit
 set prog=1
